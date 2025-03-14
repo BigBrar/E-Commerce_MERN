0558b46b0cf46b15762f940a4fe516fe2e2de73b
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2025-03-14 12:50:35 +0530

    final implementation with razorpay support

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -26,6 +26,35 @@ const PlaceOrder = () => {
     setFormdata(data=>({...data,[name]:value}))
   }
 
+  const initPay = (order)=>{
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Order Payment',
+      description: 'Order Payment',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async(response)=>{
+        console.log(response);
+        try {
+          const {data} = await axios.post(backendUrl + '/api/order/verifyRazorpay',response, {headers:{token}})
+          if (data.success){
+            navigate('/orders')
+            setCartItems({})
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error(error)
+          
+        }
+        
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+  }
+
   const onSubmitHandler = async(e)=>{
     e.preventDefault()
     try {
@@ -61,6 +90,16 @@ const PlaceOrder = () => {
           }else{
             toast.error(response.data.message)
           }
+          break;
+        
+        case 'razorpay':
+          const responseRazorpay = await axios.post(backendUrl+'/api/order/razorpay',orderData, {headers:{token}})
+
+          if (responseRazorpay.data.success){
+            initPay(responseRazorpay.data.order);
+            
+          }
+
           break;
 
         default:
